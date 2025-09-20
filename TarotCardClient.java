a8f25dd01236c98784096de40e79c51b997eab4a
Zion <158814450+ziotica@users.noreply.github.com>
2025-09-21 11:35:46 +1200

    Uploaded client and server V1

---
+++ TarotCardClient.java
@@ -0,0 +1,59 @@
+//opens TCP socket and connects to server
+// using arbitrary ports and our own protocol
+// server name/IP and port arguments given by user as cmd line args
+
+// Open connection to server on the port and IP supplies
+
+/* When client gets a response from the server:
+- make it pretty
+- print it out
+- close the socket */
+
+// optionally, you can also include the card images, as well as the text
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+  
+    // Initialize socket and input/output streams
+    private Socket s = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
+
+    // Constructor to put IP address and port
+    public Client(String addr, int port)
+    {
+        // Establish a connection
+        try {
+            s = new Socket(addr, port);
+            System.out.println("Connected"); // remove after testing
+
+            input = new ObjectInputStream(socket.getInputStream());
+
+            String reading = (String) input.readObject();
+
+            for (String card : reading) {
+                System.out.println(card);
+            }
+        }
+        catch (UnknownHostException u) {
+            System.out.println(u);
+            return;
+        }
+        catch (IOException i) {
+            System.out.println(i);
+            return;
+        }
+    }
+
+    public static void main(String[] args) {
+        Client c = new Client(args[0], args[1]); // change to cmd line input
+    }
+}
+
+
+
+
+/* sources used:
+Socket Programming in Java (https://www.geeksforgeeks.org/java/socket-programming-in-java/) */
---
+++ TarotCardServer.java
@@ -0,0 +1,90 @@
+// create TCP server socket (choose a port number)
+
+/* server loops forever around an accept method call
+which sleeps server until connection request from client */
+
+/* once connection request accepted - 
+host OS creates a worker socket (to deal with the client and thread)
+randomly select 3 tarot cards without replacement
+write the response back to client via outstream of worker socket */
+
+// close client connection, and loop back to waiting for accept request
+
+// new client connections on different worker sockets
+
+// Demonstrating Server-side Programming
+import java.net.*; // for socket functions
+import java.io.*; // for worker outstream
+import java.util.*; // for my return reading. i could do it cleaner but i like array lists
+
+public class Server {
+
+    // Initialize socket and input stream
+    private Socket s = null;
+    private ServerSocket ss = null;
+    private DataOutputStream output = null;
+
+    private String[] cards = {"The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor", "The Hierophant", 
+    "The Lovers", "The Chariot", "Strength", "The Hermit", "Wheel of Fortune", "Justice", "The Hanged Man", "Death", 
+    "Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun", "Judgement", "The World"};
+
+    // Constructor with port
+    public Server(int port) {
+
+        // Starts server and waits for a connection
+        try
+        {
+            ss = new ServerSocket(port); // worker socket
+            System.out.println("Server started"); //keep for debugging
+
+            System.out.println("Waiting for a client ..."); // keep for debugging
+
+            s = ss.accept();
+            System.out.println("Client accepted"); // keep for debugging
+
+            // Server returns three randomly selected cards         
+
+            ArrayList<String> reading = drawCards(3);
+
+	        // send client the arraylist
+            output = new DataOutputStream(socket.getOutputStream(), true);
+           
+            output.writeOutput(reading);        
+
+            System.out.println("Closing connection"); // keep for debugging
+
+            // Close connection
+            s.close();
+            output.close();
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+    }
+
+    public void drawCards(int n){ // return n cards, randomly selected, without repetition
+    // selects n indices of tarot cards, stored in order using a hashset
+    	Random random = new Random();
+    	ArrayList<String> reading = new ArrayList<>();
+
+    	while (reading.size() =< n) { // selection repeats size = 3
+           Integer next = random.nextInt(cards.length);
+
+           if(!reading.contains(cards[next])){
+            reading.add(cards[next]);
+        }
+    }
+
+    return reading;
+}
+
+public static void main(String args[])
+{
+        Server s = new Server(54321); //initialises server on chosen port, 54321
+    }
+}
+
+/* sources used:
+Socket Programming in Java (https://www.geeksforgeeks.org/java/socket-programming-in-java/) 
+Creating Random Numbers With No Duplicates in Java (https://www.baeldung.com/java-unique-random-numbers) */
