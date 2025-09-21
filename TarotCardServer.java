903e98eda130d0b6875607c5e0e9cda82cc03145
Zion <158814450+ziotica@users.noreply.github.com>
2025-09-21 16:59:29 +1200

    Update TarotCardServer.java

--- TarotCardServer.java
+++ TarotCardServer.java
@@ -5,7 +5,8 @@ import java.util.*;
 public class TarotCardServer {    
     //card deck
     private static String[] cards = {"The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor", "The Hierophant", 
-    "The Lovers"};
+    "The Lovers", "The Chariot", "Strength", "The Hermit", "Wheel of Fortune", "Justice", "The Hanged Man", "Death", 
+    "Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun", "Judgement", "The World"};
 
     public static void main(String[] args) throws Exception
     {
@@ -48,6 +49,7 @@ Socket Programming in Java
 (https://www.geeksforgeeks.org/java/socket-programming-in-java/)
 How to Create a Socket at a Specific Port in Java?
 (https://www.geeksforgeeks.org/java/how-to-create-a-socket-at-a-specific-port-in-java/) */
+
 
 
 
