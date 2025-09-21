5918b90d5d4164abf583f476035ca2feb73e5b76
Zion <158814450+ziotica@users.noreply.github.com>
2025-09-21 15:45:29 +1200

    Add files via upload

---
+++ TarotClientV2.java
@@ -0,0 +1,35 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class TarotCardClient {
+
+    private Socket s = null;
+    private ObjectInputStream in = null;
+
+    public static void main(String[] args) throws Exception
+    {
+        try {
+            // create client socket at given address and port
+            s = new Socket(args[0], Integer.parseInt(args[1]));
+
+            in = new ObjectInputStream(s.getInputStream());
+
+            ArrayList<String> reading = in.readObject();
+
+            for(String card : reading){
+                System.out.println(card);
+            }
+
+        }
+        catch (Exception e) {
+            return;
+        }
+    }
+}
+
+/* Sources used:
+Socket Programming in Java 
+(https://www.geeksforgeeks.org/java/socket-programming-in-java/)
+How to Create a Socket at a Specific Port in Java?
+(https://www.geeksforgeeks.org/java/how-to-create-a-socket-at-a-specific-port-in-java/) */
