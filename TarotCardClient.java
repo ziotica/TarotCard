450bffa287f3bec6140238d3e3264aa891f98226
Zion <158814450+ziotica@users.noreply.github.com>
2025-09-21 16:29:28 +1200

    Update TarotCardClient.java

--- TarotCardClient.java
+++ TarotCardClient.java
@@ -10,9 +10,7 @@ public class TarotCardClient {
             Socket s = new Socket(args[0], Integer.parseInt(args[1]));
             ObjectInputStream in = new ObjectInputStream(s.getInputStream());
 
-            Object object = in.readObject();
-            List reading = new ArrayList<Object>();
-            reading.add(object);
+            ArrayList<String> reading = (ArrayList<String>) in.readObject();
             
             for(String card : reading){
                 System.out.println(card);
@@ -29,5 +27,6 @@ Socket Programming in Java
 (https://www.geeksforgeeks.org/java/socket-programming-in-java/)
 How to Create a Socket at a Specific Port in Java?
 (https://www.geeksforgeeks.org/java/how-to-create-a-socket-at-a-specific-port-in-java/) */
+
 
 
