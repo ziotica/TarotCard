2621ef9c0658b0397303bbc411a411ec29963baf
Zion <158814450+ziotica@users.noreply.github.com>
2025-09-21 15:32:19 +1200

    Update TarotCardServer.java

--- TarotCardServer.java
+++ TarotCardServer.java
@@ -2,7 +2,6 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-// Class
 public class TarotCardServer {
 
     //initialise sockets
@@ -14,59 +13,41 @@ public class TarotCardServer {
     "The Lovers", "The Chariot", "Strength", "The Hermit", "Wheel of Fortune", "Justice", "The Hanged Man", "Death", 
     "Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun", "Judgement", "The World"};
 
-    // Main driver method
     public static void main(String[] args) throws Exception
     {
-
-        // Try block to check for exceptions
         try {
-            // Creating an object of ServerSocket class
-            // with the custom port number - 80
+            // create server socket with given port number
             ServerSocket ss = new ServerSocket(Integer.parseInt(args[0]));
 
-            // Display commands for better readability
-            System.out.println("Server started");
-            System.out.println("Waiting for a client ...");
-
-            // Here it will wait for any client which wants
-            // to get connected to this server
-
-            // Establishing a connection
-            // using accept() method()
+            // wait until client connects
             Socket s = ss.accept();
 
             // Display message
             System.out.println(
                 "Client accepted through the port number: "
                 + ss.getLocalPort());
-
-            // getLocalPort() function returning the port
-            // number which is being used
-
+            
+            // randomly select the past, present, and future cards
+            // without replacement, fixed size of 3
             ArrayList<String> reading = new ArrayList<>();
             Random random = new Random();
 
             while(reading.size() <= 3) {
                 Integer next = random.nextInt(cards.length);
-
                 if(!reading.contains(cards[next])){
                     reading.add(cards[next]);
                 }
             }
-
+            
+            // sent client the reading
             ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
-
             out.writeObject(reading);
+            
+            s.close();
+            out.close();
         }
-
-        // Catch block to handle for exceptions
         catch (Exception e) {
-
-            // Simply return/exit
             return;
         }
     }
-
 }
-
-
